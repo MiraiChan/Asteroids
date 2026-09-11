a2faba500eda556d9bc1fc954d155dde46cf709a
Almira Khafizova <mimirka80@gmail.com>
2026-09-11 18:36:32 +0200

    Add player and connect it

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 import pygame
@@ -7,7 +8,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+ 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -20,6 +24,9 @@ def main():
                 return
         
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
